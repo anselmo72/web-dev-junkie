fa48cf7c4f930f2c2b2f023a349d1c0a4cde601b
anselmo72 <contacto@gmail.com>
2023-02-24 19:33:16 +0000

    One more commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,22 @@
 import express, { Request, Response } from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import { config } from 'dotenv'
 const app = express()
 
 config()
 
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 import Deck from './models/Deck'
 
 const PORT = 5000
+
+app.get('/decks', async (req: Request, res: Response) => {
+  const decks = await Deck.find()
+  res.json(decks)
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.send('gg')
